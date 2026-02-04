d265715286bc34c70f88628fd45561e6b5580a09
Perpulco <pupshev16@gmail.com>
2026-02-04 22:35:29 +0500

    Initial commit

---
+++ src/test/java/useful/pupshev/UsefulBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package useful.pupshev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulBackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
