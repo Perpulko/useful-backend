ba8800b0634c911287bc78ea7d1919bd386cb36f
Perpulco <pupshev16@gmail.com>
2026-02-06 20:24:08 +0500

    useful-backend v.1.0.3

--- src/main/java/useful/pupshev/client/CsvClient.java
+++ src/main/java/useful/pupshev/client/CsvClient.java
@@ -7,6 +7,8 @@ import java.nio.file.Path;
 public class CsvClient {
   public static final String ULRCLASSIFICATORS = "https://classifikators.ru/assets/downloads/oktmo/oktmo.csv";
   public static final String ULRROSSTAT = "https://rosstat.gov.ru/opendata/7708234640-okfs/data-20231101T1111-structure-20180326T1603.csv";
+  public static final String URLCLASSIFICATORSXLS = "https://classifikators.ru/assets/downloads/oktmo/oktmo.xlsx";
   public Path targetPath = Path.of("data1.csv");
+  public Path targetPath2 = Path.of("dataXlsx.xlsx");
 
 }
--- src/main/java/useful/pupshev/controller/CsvAndXslxDownloader.java
+++ src/main/java/useful/pupshev/controller/CsvAndXslxDownloader.java
@@ -1,13 +1,17 @@
 package useful.pupshev.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import useful.pupshev.client.CsvClient;
 import useful.pupshev.service.CsvDownloaderService;
 import static useful.pupshev.client.CsvClient.ULRCLASSIFICATORS;
+import static useful.pupshev.client.CsvClient.URLCLASSIFICATORSXLS;
 
 @RestController
 @RequestMapping("api/v1")
+@Tag(name = "Скачивание файлов", description = "Скачивание файлов из интернета по ссылке")
 public class CsvAndXslxDownloader {
   private final CsvDownloaderService csvDownloaderService;
   private final CsvClient csvClient;
@@ -17,11 +21,13 @@ public class CsvAndXslxDownloader {
     this.csvClient = csvClient;
   }
 
-  @GetMapping("/classificators")
+  @GetMapping("/downloadFile")
   @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Operation(summary = "Скачивание файла")
   public void setCsvDownloader() {
       try {
-          csvDownloaderService.downloadCsv(ULRCLASSIFICATORS, csvClient.targetPath);
+//          csvDownloaderService.downloadCsv(ULRCLASSIFICATORS, csvClient.targetPath);
+          csvDownloaderService.downloadCsv(URLCLASSIFICATORSXLS, csvClient.targetPath2);
       } catch (Exception e) {
           throw new RuntimeException(e);
       }
